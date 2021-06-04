411bee5e0d5cc08f947fa9dd3390aad6bc678398
Adam Rolczyk <adamr6561@gmail.com>
2021-06-04 21:40:03 +0200

    typescript

--- _rfctr/main.js
+++
@@ -1,7 +0,0 @@
-import "./scss/index.scss";
-import initSmoothScroll from "./scripts/smoothScroll";
-import SphereShader from "./scripts/sphereShader";
-
-// For smoothscroll wrap everything in main with <div id="scroll-container"> element.
-// initSmoothScroll();
-// new SphereShader(".splash__shader-container").init();
