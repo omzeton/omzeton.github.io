aa11152541efa8b590adc35a3ab3e6fb90ac02d2
Adam Rolczyk <adamr6561@gmail.com>
2021-06-11 21:09:01 +0200

    shadertoy template

--- _rfctr2/src/glsl/fragment.glsl
+++ _rfctr2/src/glsl/fragment.glsl
@@ -4,5 +4,7 @@ varying vec2 vUv;
 varying vec3 vPosition;
 
 void main() {
-    gl_FragColor = vec4(vUv, 0., 1.);
+    vec2 uv = gl_FragCoord.xy / resolution.xy;
+    vec3 col = 0.5 + 0.5 * cos(time + uv.xyx + vec3(0.0, 2.0, 4.0));
+    gl_FragColor = vec4(col, 1.0);
 }
