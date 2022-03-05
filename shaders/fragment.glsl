d64184bf0445b6f144d0ca90870cb441de479df5
Adam Rolczyk <adamrolczyk@gmail.com>
2022-03-05 21:55:57 +0100

    add box patterns

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,7 +1,9 @@
 uniform vec2 u_resolution;
 uniform float u_time;
 
+const vec3 C1 = vec3(1.0, 0.972, 0.909);
+
 void main() {
-    vec2 uv = (gl_FragCoord.xy) / min(u_resolution.x, u_resolution.y);
-    gl_FragColor = vec4(1.0, 0.972, 0.909, 1.0);
+    vec2 uv = gl_FragCoord.xy / u_resolution;
+    gl_FragColor = vec4(C1, 1.0);
 }
