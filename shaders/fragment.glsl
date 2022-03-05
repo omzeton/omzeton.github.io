f9b80b7e4fc7786902db09402d64205b8c7c25ac
Adam Rolczyk <adamrolczyk@gmail.com>
2022-03-05 16:10:41 +0100

    header position

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,11 +1,7 @@
 uniform vec2 u_resolution;
 uniform float u_time;
 
-float fn(vec2 a, vec2 b) {
-    float v = sin(dot(b + a, a + b));
-    return cos(v * dot(a, b)) + 0.5;
-}
-
 void main() {
+    vec2 uv = (gl_FragCoord.xy) / min(u_resolution.x, u_resolution.y);
     gl_FragColor = vec4(1.0, 0.972, 0.909, 1.0);
 }
